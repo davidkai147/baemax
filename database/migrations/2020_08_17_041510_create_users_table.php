a3f090cb30c72d101364a28426ef50e299c83591
GBU_DanhNC <danh.nguyen@dsquare.com.vn>
2020-08-17 11:20:43 +0700

    migrations

---
+++ database/migrations/2020_08_17_041510_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+
+            $table->id();
+            $table->tinyInteger('status')->nullable();
+            $table->tinyInteger('type');
+            $table->string('name')->unique();
+            $table->string('email')->unique();
+            $table->string('phone_number');
+            $table->string('password')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->index('created_at');
+            $table->index('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ database/migrations/2020_08_17_041615_create_areas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAreasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('areas', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+
+            $table->id();
+            $table->tinyInteger('level');
+            $table->bigInteger('parent_id')->unsigned();
+            $table->string('name')->nullable();
+            $table->timestamps();
+
+            $table->foreign('parent_id')->references('id')->on('areas')
+                ->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('areas');
+    }
+}
